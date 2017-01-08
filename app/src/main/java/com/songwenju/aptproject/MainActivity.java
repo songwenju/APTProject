ffdcf284b837b1d432b073af8b28fc2ee40b2878
songwenju <hgdswj@163.com>
2017-01-08 21:48:28 +0800

    Initial commit

---
+++ app/src/main/java/com/songwenju/aptproject/MainActivity.java
@@ -0,0 +1,16 @@
+package com.songwenju.aptproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.AutoCreat;
+
+@AutoCreat
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
