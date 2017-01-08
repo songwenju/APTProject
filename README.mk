6a4afca8e885847e340cf95295d42a300f1ec80e
songwenju <hgdswj@163.com>
2017-01-08 22:06:45 +0800

    add the README

---
+++ README.mk
@@ -0,0 +1,6 @@
+####APT Project
+随着一些如ButterKnife，dagger等的开源注解框架的流行，APT的概念也越来越被熟知。
+APT英文全称：Android annotation process tool是一种处理注释的工具，
+它对源代码文件进行检测找出其中的Annotation，使用Annotation进行额外的处理。
+这里是一个关于使用APT自定义注解的一个例子。详尽的内容请参考以下博客：
+http://www.jianshu.com/p/94979c056b20
